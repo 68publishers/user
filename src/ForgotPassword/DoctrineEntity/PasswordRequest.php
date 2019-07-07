2465bc69588c5cec5f67aabf720732cf58683023
Tomáš Glawaty <tomasglawaty@icloud.com>
2019-07-08 01:03:55 +0200

    Removed `final` from entity `PasswordRequest` - Doctrine can't create proxy object if entity is marked as final.

--- src/ForgotPassword/DoctrineEntity/PasswordRequest.php
+++ src/ForgotPassword/DoctrineEntity/PasswordRequest.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\HasLifecycleCallbacks
  */
-final class PasswordRequest implements IPasswordRequest
+class PasswordRequest implements IPasswordRequest
 {
 	use Nette\SmartObject;
 
