3a4281146db98922fd99d5ddbe053ee5b89dc267
tg666 <tomasglawaty@icloud.com>
2020-11-29 23:12:18 +0100

    Reset password - autologin
    - added method `ResetPasswordControl::setAutoLogin()`
    - an interface `UserInterface` from `ForgotPassword` namespace now extends from the interface `UsernameAccessorInterface`
    - the user is automatically logged in when a password is reset if autologi is enabled

--- src/ForgotPassword/Entity/UserInterface.php
+++ src/ForgotPassword/Entity/UserInterface.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace SixtyEightPublishers\User\ForgotPassword\Entity;
 
 use SixtyEightPublishers\User\Common\Entity\Accessor\EmailAccessorInterface;
+use SixtyEightPublishers\User\Common\Entity\Accessor\UsernameAccessorInterface;
 use SixtyEightPublishers\User\Common\Entity\Assigner\PasswordAssignerInterface;
 use SixtyEightPublishers\User\Common\Entity\UserInterface as CommonUserInterface;
 
-interface UserInterface extends CommonUserInterface, EmailAccessorInterface, PasswordAssignerInterface
+interface UserInterface extends CommonUserInterface, EmailAccessorInterface, UsernameAccessorInterface, PasswordAssignerInterface
 {
 }
